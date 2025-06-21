2119f773b9e485e9ec8576603b4b94be28c40957
tello911 <65243549+tello911@users.noreply.github.com>
2025-06-21 23:25:19 +0800

    Initialize

---
+++ Python Coursera Learning/firstapp.py
@@ -0,0 +1,7 @@
+from pyspark import SparkContext
+logFile = "file:///home/hadoop/spark-2.1.0-bin-hadoop2.7/README.md"  
+sc = SparkContext("local", "first app")
+logData = sc.textFile(logFile).cache()
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
